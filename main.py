1e058e45bc25d620d7406649e454b718ec9644ef
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2025-03-04 21:09:17 -0500

    converter endpoint added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 def home():
     return "Hola"
+
+
+@app.get("/item/{id}")
+def query_path_params(id: int, name: str):
+    return f"Hello {name}, your id is {id}"
+
+
+@app.post("/convertidor")
+async def convertidor(request: Request):
+    body = await request.json()
+    amount = body.get("name")
+    to_currency = body.get("amount")
